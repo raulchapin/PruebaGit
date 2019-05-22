8065c067e4fd3e2f977756c22f81e4d1fa868af4
raulchapin <raulzazm10@gmail.com>
2019-05-22 20:58:40 +0200

    he pasado de 10 a 100

--- PruebaGit/src/pruebagit/PruebaGit.java
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -16,7 +16,7 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.println(i);
         }
     }
